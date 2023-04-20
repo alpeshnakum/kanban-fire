f340ef304f7b23f3cbdb4e86616bad5c7944b43a
alpeshnakum <19bmiit068@gmail.com>
2023-04-20 11:49:33 +0530

    changed gitignore file

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDOFT0UTpOzOjqvASVE4RSkalilY-ve2Iw",
+        authDomain: "kanbanfire-6e21b.firebaseapp.com",
+        projectId: "kanbanfire-6e21b",
+        databaseURL: "https://kanbanfire-6e21b.firebaseio.com",
+        storageBucket: "kanbanfire-6e21b.appspot.com",
+        messagingSenderId: "875998849945",
+        appId: "1:875998849945:web:3224d75c37eb2cef454108",
+        measurementId: "G-J8EB7R2QKD",
+    },
+};
